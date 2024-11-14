dee5a182ace84e14c0bdc93df4ff41adbe8191a4
Junior guinier <guinier95.js@gmail.com>
2024-11-14 18:31:43 +0100

    recuperation des données json

---
+++ scripts/pages/index.js
@@ -0,0 +1,24 @@
+async function getPhotographers() {
+  let datas = await fetch("../data/photographers.json");
+  const photographers = await datas.json();
+  console.log(photographers);
+  return photographers;
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
